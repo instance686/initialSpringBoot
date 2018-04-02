268c30ee388a7ac26ed2cd341a385c5673634a13
ayush <ayush.cir@gmail.com>
2018-04-02 16:16:35 +0530

    DevTools Added

--- src/main/java/com/example1/help/ctrl/MyController.java
+++ src/main/java/com/example1/help/ctrl/MyController.java
@@ -2,13 +2,19 @@ package com.example1.help.ctrl;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
-	@RequestMapping(value="/welcome",method=RequestMethod.POST)
+	@RequestMapping(value="/welcome",method=RequestMethod.GET)
 	public String welcomeMsg() {
 		return "Welcome to spring boot app";
+	}
+	
+	@RequestMapping("/getUser")
+	public String getUserName(@RequestParam(value="myname") String name) {
+		return "Welcome to first attempt:"+name;
 	}
 
 }
