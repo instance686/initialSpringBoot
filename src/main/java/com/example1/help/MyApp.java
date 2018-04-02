19c64ab914812f8ba14d8b320f8a84b0284ab1d9
ayush <ayush.cir@gmail.com>
2018-04-02 20:26:13 +0530

    YML inclusion and DEV seperation

--- src/main/java/com/example1/help/MyApp.java
+++ src/main/java/com/example1/help/MyApp.java
@@ -10,13 +10,20 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
+import com.example1.help.ctrl.Contact;
 import com.example1.help.ctrl.Database;
 
 @SpringBootApplication
 @Configuration
-@PropertySource("classpath:database.properties")
+//@PropertySource({"classpath:database.properties","classpath:email.properties"})
+/*@PropertySources({
+		@PropertySource("classpath:database.properties"),
+		@PropertySource("classpath:email.properties")
+})*/
+
 public class MyApp {
 
 	public static void main(String[] args) {
@@ -24,14 +31,30 @@ public class MyApp {
 		
 		ConfigurableApplicationContext ctx=SpringApplication.run(MyApp.class, args);
 		Database db=(Database)ctx.getBean(Database.class);
+		Contact con=(Contact)ctx.getBean(Contact.class);
 		System.out.println("Name:"+db.getName());
 		System.out.println("Password"+db.getPassword());
+		System.out.println("Email:"+con.getEmail());
+		System.out.print("Number:"+con.getNumber());
 	}
 	
 	@Value("${channel.name}")
 	public String name;
 	@Value("${channel.password}")
 	public String password;
+	
+	@Value("${contact.email}")
+	public String email;
+	@Value("${contact.number}")
+	public String number;
+	
+	@Bean
+	public Contact getDet() {
+		Contact con=new Contact();
+		con.setEmail(email);
+		con.setNumber(number);
+		return con;
+		}
 	
 	@Bean
 	public Database getDetails() {
@@ -41,11 +64,11 @@ public class MyApp {
 		return db;
 	}
 	
-	@Bean
+	/*@Bean
 	public static PropertySourcesPlaceholderConfigurer property() 
 	{
 		return new PropertySourcesPlaceholderConfigurer();
 		
-	}
+	}*/
 
 }
---
+++ src/main/java/com/example1/help/ctrl/Contact.java
@@ -0,0 +1,21 @@
+package com.example1.help.ctrl;
+
+public class Contact {
+
+	private String email;
+	private String number;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	
+	
+}
